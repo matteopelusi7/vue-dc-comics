d83619a6d201c0188d621cf0ef1c3a1ae784c61e
matteopelusi7 <pelusimatteo02@gmail.com>
2022-03-08 16:19:49 +0100

    Add header

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,36 @@ export default {
 </script>
 
 <style lang="scss">
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+img {
+  max-width: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+  
+ul {
+  list-style: none;
+}
+  
+a {
+  text-decoration: none;
+  color: currentColor;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1024px;
+}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,95 @@
 <template>
   <header>
-    
+    <div class="container">
+      <figure class="logo">
+        <img src="../assets/img/dc-logo.png" alt="">
+      </figure>
+      <nav>
+        <ul class="list-nav">
+          <li v-for="(link, i) in links" :key="i">
+            <a :href="link.href">{{ link.text }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
   name: 'HeaderContent',
+  data() {
+    return {
+      links: [
+        {
+          text: 'Characters',
+          href: '#'
+        },
+        {
+          text: 'Comics',
+          href: '#'
+        },
+        {
+          text: 'Movies',
+          href: '#'
+        },
+        {
+          text: 'Tv',
+          href: '#'
+        },
+        {
+          text: 'Games',
+          href: '#'
+        },
+        {
+          text: 'Collectibles',
+          href: '#'
+        },
+        {
+          text: 'Video',
+          href: '#'
+        },
+        {
+          text: 'Fans',
+          href: '#'
+        },
+        {
+          text: 'News',
+          href: '#'
+        },
+        {
+          text: 'Shop',
+          href: '#'
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+
+.container {
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 30px 10px;
+
+  .logo {
+    width: 80px;
+  }
+
+  .list-nav {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    flex-wrap: wrap;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+
+}
 
 </style>
